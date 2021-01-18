d6ad87af87322397c52e2e6146f82f403bf57c01
shard520 <65344196+shard520@users.noreply.github.com>
2021-01-18 23:46:16 +0100

    Add navigation and put in placeholder HTML for mobile navigation - hidden with display:none, adjust insertNav.js for new BEM selectors, adjust active nav highlight selector to use chained selectors to override selector with pseudo class.

--- js/insertNav.js
+++ js/insertNav.js
@@ -1,54 +1,72 @@
 (function () {
 	const fullTitle = document.querySelector("title").innerText;
 	const title = fullTitle.replace(' - Moroccan Adventures', '')
-	const navBar = document.querySelector(".navbar");
+	const navBar = document.querySelector(".navigation");
 	const srOnly = '<span class="sr-only">(current)</span>';
 
 	function addActive() {
-		const navList = document.getElementsByClassName("navbar-nav")[0].children;
+		const navList = document.getElementsByClassName("navigation__list")[0].children;
 
 		for (i = 0; i < navList.length; i++) {
 			if (navList[i].innerHTML.includes(title)) {
 				navList[i].innerHTML =
-					'<a class="nav-link" href="#">' + title + " " + srOnly + "</a>";
-				navList[i].classList = "nav-item highlight";
+					'<a class="navigation__link" href="#">' + title + " " + srOnly + "</a>";
+				navList[i].firstElementChild.classList.add("navigation__link--highlight");
+			}
+		}
+
+		const dropdownNavList = document.getElementsByClassName("navigation__dropdown-list")[0].children;
+
+		for (i = 0; i < dropdownNavList.length; i++) {
+			if (dropdownNavList[i].innerHTML.includes(title)) {
+				dropdownNavList[i].innerHTML =
+					'<a class="navigation__dropdown-link" href="#">' + title + " " + srOnly + "</a>";
+				dropdownNavList[i].firstElementChild.classList.add("navigation__dropdown-link--highlight");
 			}
 		}
 	}
 
 	navBar.innerHTML = `
-	<button	
-		class="navbar-toggler" 
-		type="button" 
-		data-toggle="collapse" 
-		data-target="#navbarToggler" 
-		aria-controls="navbarToggler" 
-		aria-expanded="false"
-		aria-label="Toggle navigation"
-	>
-		<span class="navbar-toggler-icon"></span>
-	</button>
-	<a class="navbar-brand" href="#">Moroccan Adventures</a>
-	<div class="collapse navbar-collapse" id="navbarToggler">
-		
-		<ul class="navbar-nav mx-auto mr-auto mt-2 mt-lg-0">
-			<li class="nav-item">
-				<a class="nav-link black-text" href="index.html">Home</a>
+	<input type="checkbox" class="navigation__checkbox" id="navi-toggle">
+	<nav class="navigation__dropdown-menu">
+		<ul class="navigation__dropdown-list">
+			<li class="navigation__dropdown-item">
+				<a href="index.html" class="navigation__dropdown-link">Home</a>
 			</li>
-			<li class="nav-item">
-				<a class="nav-link black-text" href="erg-chebbi.html">Erg Chebbi</a>
+			<li class="navigation__dropdown-item">
+				<a href="erg-chebbi.html" class="navigation__dropdown-link">Erg Chebbi</a>
 			</li>
-			<li class="nav-item">
-				<a class="nav-link black-text" href="ait-benhaddou.html">Aït Benhaddou</a>
+			<li class="navigation__dropdown-item">
+				<a href="ait-benhaddou.html" class="navigation__dropdown-link">Aït Benhaddou</a>
 			</li>
-			<li class="nav-item">
-				<a class="nav-link black-text" href="chefchaouen.html">Chefchaouen</a>
+			<li class="navigation__dropdown-item">
+				<a href="chefchaouen.html" class="navigation__dropdown-link">Chefchaouen</a>
 			</li>
-			<li class="nav-item">
-			<a class="nav-link black-text" href="contact.html">Contact</a>
-		</li>
+			<li class="navigation__dropdown-item">
+				<a href="contact.html" class="navigation__dropdown-link">Contact</a>
+			</li>
 		</ul>
-	</div>
+	</nav>
+	<label for="navi-toggle" class="navigation__button" tabindex="1" aria-label="Toggle navigation">
+		<span class="navigation__icon">&nbsp;</span>
+	</label>
+	<ul class="navigation__list">
+		<li class="navigation__item">
+			<a href="index.html" class="navigation__link navigation__link--highlight">Home</a>
+		</li>
+		<li class="navigation__item">
+			<a href="erg-chebbi.html" class="navigation__link">Erg Chebbi</a>
+		</li>
+		<li class="navigation__item">
+			<a href="ait-benhaddou.html" class="navigation__link">Aït Benhaddou</a>
+		</li>
+		<li class="navigation__item">
+			<a href="chefchaouen.html" class="navigation__link">Chefchaouen</a>
+		</li>
+		<li class="navigation__item">
+			<a href="contact.html" class="navigation__link">Contact</a>
+		</li>
+	</ul>
 	`;
 
 	addActive();
